de598559c9bafc26886a797461db9954b6610163
RKorets <93273641+RKorets@users.noreply.github.com>
2022-06-03 13:17:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,35 +18,47 @@ class ChromeOptions:
         self.option.add_argument(f'user-data-dir=/Users/romankorets/Downloads/chrome_cash {profile_number}')
         self.option.add_argument(f'--profile-directory=Profile {profile_number}')
         
-def like(number):
+def like(number, lst):
     account = ChromeOptions(f'{number}')
     print(f"pid={os.getpid()}, x={number} ")
     with Chrome(service=Service(ChromeDriverManager().install()), options=account.option) as driver:
-        with open("di_helen__.txt", "r") as file:
-            lines = file.readlines()
-            count_account = 5
-            for users in lines:
-                if count_account:
-                    count_account -= 1
-                    driver.get(users)
-                    sleep(3)
-                    try:
-                        # driver.find_element(by=By.CLASS_NAME, value='_9AhH0').click()
-                        driver.find_element(by=By.CLASS_NAME, value='_ac7v').click()
-                        sleep(3)
-                        driver.find_element(by=By.CLASS_NAME, value="_aamw").click()
-                        sleep(1)
-                        print(f"Success {users}")
-                    except NoSuchElementException:
-                        with open("not_like_profile.txt", "a+") as files:
-                            files.write(users)
-                        print("Error like: NoSuchElementException")
-                        
-                        
+        for users in lst:
+            driver.get(users)
+            sleep(3)
+            try:
+                # driver.find_element(by=By.CLASS_NAME, value='_9AhH0').click()
+                driver.find_element(by=By.CLASS_NAME, value='_ac7v').click()
+                sleep(3)
+                driver.find_element(by=By.CLASS_NAME, value="_aamw").click()
+                sleep(1)
+                print(f"Success {users}")
+            except NoSuchElementException:
+                with open(f"not_like_profile-{number}.txt", "a+") as files:
+                    files.write(users)
+                print("Error like: NoSuchElementException")
+
+
+
+def create_list():
+    result = []
+    with open("di_helen__.txt", "r") as file:
+        lines = file.readlines()
+        count_account = 5
+        for users in lines:
+            if count_account:
+                count_account -= 1
+                result.append(users)
+    del lines[0:5]
+    with open("di_helen__.txt", "w+") as file:
+        for line in lines:
+            file.write(line)
+    return result
+
+
 def multip():
     pool = Pool(processes=3)
     for i in range(0, 3):
-        pool.apply_async(like, args=(i,))
+        pool.apply_async(like, args=(i, create_list(),))
     pool.close()
     pool.join()
     
